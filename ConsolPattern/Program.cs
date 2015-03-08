e7649e08ee0564d604ce251e3061b0f8aa793e01
Morigun <GunGrayv@gmail.com>
2015-03-08 11:20:19 +0300

    Delegate

---
+++ ConsolPattern/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolPattern
+{
+    
+    class Program
+    {
+        public delegate void MyEventHandler(); // Пользовательский тип делегата
+
+        public class Subject
+        {
+            public Subject() {}
+        
+            public MyEventHandler MyEvent; //Событие
+
+            public void RaiseEvent()
+            {
+                MyEventHandler ev = MyEvent;
+                if (ev != null)
+                    ev();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Subject s = new Subject();
+            s.MyEvent += () => Console.WriteLine("Hello D"); // присоеденить делегат
+            s.MyEvent += () => Console.WriteLine("Hello World"); // присоеденить делегат
+            s.RaiseEvent(); //вызвать все делегаты
+            Console.ReadKey();
+        }
+    }
+}
